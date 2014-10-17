091fca7442737ed5d6979a048802101897a66a83
aliilyas <alikhojayev@gmail.com>
2014-10-17 22:07:34 +0600

    file upload first steps

--- web/js/index.js
+++ web/js/index.js
@@ -1,3 +1,109 @@
+var styleColor = [
+    {
+        "featureType": "water",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#193341"
+            }
+        ]
+    },
+    {
+        "featureType": "landscape",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#2c5a71"
+            }
+        ]
+    },
+    {
+        "featureType": "road",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#29768a"
+            },
+            {
+                "lightness": -37
+            }
+        ]
+    },
+    {
+        "featureType": "poi",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#406d80"
+            }
+        ]
+    },
+    {
+        "featureType": "transit",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#406d80"
+            }
+        ]
+    },
+    {
+        "elementType": "labels.text.stroke",
+        "stylers": [
+            {
+                "visibility": "on"
+            },
+            {
+                "color": "#3e606f"
+            },
+            {
+                "weight": 2
+            },
+            {
+                "gamma": 0.84
+            }
+        ]
+    },
+    {
+        "elementType": "labels.text.fill",
+        "stylers": [
+            {
+                "color": "#ffffff"
+            }
+        ]
+    },
+    {
+        "featureType": "administrative",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "weight": 0.6
+            },
+            {
+                "color": "#1a3541"
+            }
+        ]
+    },
+    {
+        "elementType": "labels.icon",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "poi.park",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#2c5a71"
+            }
+        ]
+    }
+];
+
+
 /* 
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -10,7 +116,8 @@ var map;
 function initialize() {
     var mapOptions = {
         zoom: 3,
-        center: new google.maps.LatLng(46, 76)
+        center: new google.maps.LatLng(46, 76),
+        styles: styleColor
     };
 
     map = new google.maps.Map(document.getElementById('map-canvas'),
@@ -41,7 +148,10 @@ ws.onopen = function() {
 
 ws.onmessage = function(message) {
     var lats = message.data.split("=")[0];
-    drawMessage(message.data.split("=")[1], "aaa");
+    var str = message.data.split("=")[1];
+    var messageStr = str.split(":")[0];
+    var from = str.split(":")[1];
+    drawMessage(messageStr, from);
     var latitude = parseFloat(lats.split(":")[0]);
     var longitude = parseFloat(lats.split(":")[1]);
     var myLatlng = new google.maps.LatLng(latitude, longitude);
@@ -50,6 +160,18 @@ ws.onmessage = function(message) {
         map: map,
         title: message.data.split("=")[1],
         icon: 'images/marker.png'
+    });
+    var msg = "<div>" + messageStr + "<br><br>     <span style='color:red;'>" + from + "</span></div>";
+    var infowindow = new google.maps.InfoWindow({
+        content: msg
+    });
+    
+    setInterval(function(){
+        marker.setMap(null);
+    },"6000");
+    
+    google.maps.event.addListener(marker, 'click', function() {
+        infowindow.open(map, marker);
     });
 };
 
